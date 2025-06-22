e78149df862dc946e18f10ccdae3a6a63c0cdacc
BNJ808 <134887751+BNJ808@users.noreply.github.com>
2025-06-22 20:51:45 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/nom-de-votre-repo/', // Remplacez par le nom de votre repo GitHub
+  base: '/iron-path-journal/', // Remplacez par le nom de votre repo GitHub
   resolve: {
     alias: {
       '@': '/src'
